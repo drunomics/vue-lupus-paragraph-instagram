91276b502f97ff2d92fa6df2b10e44386101be74
Arthur Lorenz <arthur@drunomics.com>
2018-08-23 18:11:15 +0200

    Download embed script.

--- src/pg-instagram.vue
+++ src/pg-instagram.vue
@@ -23,6 +23,11 @@
       if (this.dataInstagramUrl !== '') {
         axios.get('https://api.instagram.com/oembed/?url=' + this.dataInstagramUrl).then(response => {
           this.embedHtml = response.data.html;
+          // The response HTML from instagram contains this script tag already,
+          // but via v-html it does not get executed so we need to add this element here manually.
+          const script = document.createElement('script')
+          script.src = '//www.instagram.com/embed.js'
+          document.head.appendChild(script)
         });
       }
     },
