c3b93359c53fb25b78ac5d697231db66d6f3573b
Maximilian Mikus <maximilianmikus@gmail.com>
2019-04-12 10:50:51 +0200

    LDP-160 Use vue-cli and update to new specs.

--- src/pg-instagram.vue
+++ src/pg-instagram.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="paragraph paragraph--instagram">
-    <div 
-      class="paragraph__field-media"
+    <slot name="title" />
+    <div
+      class="instagram-post"
       v-html="embedHtml"
-    >
-    </div>
+    />
   </div>
 </template>
 <script>
-  import axios from 'axios';
-  export default {
-    name: 'PgInstagram',
-    props: {
-      dataUrl: { type: String, default: () => '' },
-    },
-    data() {
-      return {
-        embedHtml: '',
-      }
-    },
-    mounted() {
-      if (this.dataUrl !== '') {
-        axios.get('https://api.instagram.com/oembed/?url=' + this.dataUrl).then(response => {
-          this.embedHtml = response.data.html;
-          // The response HTML from instagram contains this script tag already,
-          // but via v-html it does not get executed so we need to add this element here manually.
-          const script = document.createElement('script')
-          script.src = '//www.instagram.com/embed.js'
-          document.head.appendChild(script)
-        });
-      }
-    },
-  };
+import axios from 'axios'
+export default {
+  name: 'PgInstagram',
+  props: {
+    src: { type: String, default: () => '' }
+  },
+  data () {
+    return {
+      embedHtml: ''
+    }
+  },
+  mounted () {
+    if (this.dataUrl !== '') {
+      axios.get('https://api.instagram.com/oembed/?url=' + this.src).then(response => {
+        this.embedHtml = response.data.html
+        // The response HTML from instagram contains this script tag already,
+        // but via v-html it does not get executed so we need to add this element here manually.
+        const script = document.createElement('script')
+        script.src = '//www.instagram.com/embed.js'
+        script.async = true
+        document.head.appendChild(script)
+      })
+    }
+  }
+}
 </script>
